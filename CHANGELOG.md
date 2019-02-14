e2211d568d8e39ea844eee5cd7f77a564dec9551
JOSE ANGEL BERMUDEZ <joangelbepu16@gmail.com>
2019-02-14 13:54:21 -0500

    Test en windows

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
-### v2.0.6 (2019-02-13)
+### v2.0.9 (2019-02-13)
 
 Creación del comando `exito generate` para realizar la creación de los templates para aws cloud formation con el comando `exito generate templte [repository name]` y la creación de la configuración básica de los projectos con el comando `exito generate config`
 
--- src/modules/vtex/create-config-project/get-config.ts
+++ src/modules/vtex/create-config-project/get-config.ts
@@ -4,22 +4,30 @@ import { ncp } from 'ncp';
 const configDirectory: string = __dirname + '/resources';
 
 export default async () => {
-  log.info('Generating the last config for your project');
-  let firstCopy = await move(configDirectory, process.cwd() + '/');
-  if (firstCopy) {
-    log.info('files config generate succesfully!!');
-  } else {
-    log.error('error on generate files config');
+  try {
+    log.info('Generating the last config for your project');
+    let firstCopy = await move(configDirectory, process.cwd() + '/');
+    if (firstCopy) {
+      log.info('files config generate succesfully!!');
+    } else {
+      log.error('error on generate files config');
+    }
+  } catch (error) {
+    log.debug(error)
   }
 };
 
 const move = (source: string, destination: string): Promise<boolean> => {
   return new Promise((resolve, reject) => {
-    ncp(source, destination, (err: Error) => {
-      if (err) {
-        reject(false);
-      }
-      resolve(true);
-    });
+    try {
+      ncp(source, destination, (err: Error) => {
+        if (err) {
+          reject(false);
+        }
+        resolve(true);
+      });
+    } catch (error) {
+      log.debug(error)
+    }
   });
 };
